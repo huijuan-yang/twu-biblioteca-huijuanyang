c203a3dc1e7fff98c79f3e95b365e9817399b277
huijuan-yang <huijuan.yang@thoughtworks.com>
2020-08-03 21:38:22 +0800

    get rid of redundant

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,11 +1,5 @@
 package com.twu.biblioteca;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-
-
 public class BibliotecaApp {
 
     public static void main(String[] args) {
